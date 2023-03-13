09711919864113ca26f0109ba008140a1850cfd9
罗瑞东 <2185945875@qq.com>
2023-03-13 22:21:29 +0800

    Revert "论坛1首页"
    This reverts commit d1e39c5d8b91907b0c72ba4443db3ff3ca811c58.

--- src/test/java/com/example/newcoder/NewCoderApplicationTests.java
+++ src/test/java/com/example/newcoder/NewCoderApplicationTests.java
@@ -1,25 +1,11 @@
 package com.example.newcoder;
 
-import com.example.newcoder.dao.UserMapper;
-import com.example.newcoder.entity.User;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NewCoderApplicationTests
 {
-
-
-    @Autowired
-    UserMapper userMapper;
-
-    @Test
-    void testUserMapper()
-    {
-        User user = userMapper.selectById(6);
-        System.out.println(user);
-    }
 
     @Test
     void contextLoads()
